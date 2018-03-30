ff406be2304dcc51d81cddeb03d18a330e44799c
yalelikeyale <newmany89@gmail.com>
2018-03-30 15:43:54 +1100

    Delete config.js

--- config/config.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-exports.AWS_ACCESS = process.env.AWS_ACCESS || 'AKIAIBCV7QLR7MJLPJ4A';
-exports.AWS_SECRET = process.env.AWS_SECRET || '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';
-exports.PORT = process.env.PORT || 8080;
