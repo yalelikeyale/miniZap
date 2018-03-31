50de672505cdf79250f375139b338578c8a07b42
yale <newmany89@gmail.com>
2018-04-01 09:20:16 +1000

    update

--- config/config.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-exports.AWS_ACCESS = process.env.aws_access_key_id;
-exports.AWS_SECRET = process.env.aws_secret_access_key;
-exports.PORT = process.env.PORT || 8080;
--- server.js
+++ server.js
@@ -1,32 +1,47 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-const { PORT } = require('./config/config');
 const segmentRouter  = require('./routers/segmentRouter');
 const podioRouter  = require('./routers/podioRouter');
+const mongoose = require('mongoose');
+const config = require('dotenv').config()
+const PORT = process.env.PORT || 8080;
+const MLAB_URI = process.env.MLAB_URI;
+
 
 const app = express();
 
 let server;
 
-function runServer(port = PORT) {
+function runServer(databaseUrl, port = PORT) {
   return new Promise((resolve, reject) => {
-    server = app.listen(port, () => {
-      console.log(`Your app is listening on port ${port}`);
-      resolve();
-    }).on('error', err => {
-        reject(err);
-      });
+    mongoose.connect(MLAB_URI, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+        .on('error', err => {
+          mongoose.disconnect();
+          reject(err);
+        });
+    });
   });
 }
 
 function closeServer() {
-  console.log('Closing server');
-  server.close(err => {
-    if (err) {
-      return reject(err);
-    }
-    resolve();
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('Closing server');
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
   });
 }
 
