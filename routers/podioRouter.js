83e0a0d9e561133159f5915055b5d99a15c5d27a
yale <newmany89@gmail.com>
2018-03-31 22:33:48 +1100

    update

--- routers/podioRouter.js
+++ routers/podioRouter.js
@@ -27,36 +27,36 @@ router.get('/:category', (req,res)=>{
     if (req.user === undefined) {
         return res.status(403).json({message: 'Must supply valid user credentials'});
     }
-    res.status(200).send(req.query);
-    // const podio = new Podio({
-    //     authType: 'server',
-    //     clientId: process.env.podio_id,
-    //     clientSecret: process.env.podio_secret
-    // });
+    const podio = new Podio({
+        authType: 'server',
+        clientId: process.env.podio_id,
+        clientSecret: process.env.podio_secret
+    });
 
-    // // get the app ID and Token for appAuthentication
-    // const companyId = process.env.podio_company_id;
-    // const companyToken = process.env.podio_company_token;
+    // get the app ID and Token for appAuthentication
+    const companyId = process.env.podio_company_id;
+    const companyToken = process.env.podio_company_token;
 
-    // // instantiate the SDK
-    // const podio = new Podio({
-    //     authType: 'app',
-    //     clientId: clientId,
-    //     clientSecret: clientSecret
-    // });
+    // instantiate the SDK
+    const podio = new Podio({
+        authType: 'app',
+        clientId: clientId,
+        clientSecret: clientSecret
+    });
 
-    // podio.authenticateWithApp(companyId, companyToken, (err) => {
-    //   if (err) throw new Error(err);
-    //   podio.isAuthenticated().then(() => {
-    //     // Ready to make API calls in here...
-    //         podio.request('GET', '/tasks').then(function(responseData) {
-    //               // response, if available
+    podio.authenticateWithApp(companyId, companyToken, (err) => {
+      if (err) throw new Error(err);
+      podio.isAuthenticated().then(() => {
+        console.log('made it through authentication');
+        // Ready to make API calls in here...
+            // podio.request('GET', '').then(function(responseData) {
+            //       // response, if available
 
-    //         }).catch(function(err){
-    //           console.log(err);
-    //         })
-    //   }).catch(err => console.log(err));
-    // });
+            // }).catch(function(err){
+            //   console.log(err);
+            // })
+      }).catch(err => console.log(err));
+    });
 });
 
 module.exports = router;
