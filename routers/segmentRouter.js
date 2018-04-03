0413d847d2708a52334d5eff67d9a1aba47aae01
yale <newmany89@gmail.com>
2018-04-03 17:27:51 +1000

    update

--- routers/segmentRouter.js
+++ routers/segmentRouter.js
@@ -2,24 +2,49 @@ const express = require('express');
 const segRouter = express.Router();
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
+const Analytics = require('analytics-node');
+const write_key = process.env.segment_write;
 
-const WooCommerceAPI = require('woocommerce-api');
-
-const WooCommerce = new WooCommerceAPI({
-	url: 'http://example.com',
-	consumerKey: 'ck_8ed852babf969a3edf04625213bb73e0d677e352',
-	consumerSecret: 'cs_XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
-	wpAPI: true,
-	version: 'wc/v1'
-});
+const analytics = new Analytics(write_key);
 
 
 segRouter.post('/identify', jsonParser,(req,res)=>{
 
 });
 
-segRouter.post('/addtocart', jsonParser,(req,res)=>{
-
+segRouter.post('/order-completed', jsonParser,(req,res)=>{
+	const order = req.body;
+	const productsList = [];
+	for(i=0;i<order.line_items.length;i++){
+		let currentItem = order.line_items[i]
+		let lineItem = {}
+		lineItem.product_id = currentItem.product_id
+		lineItem.sku = currentItem.sku
+		lineItem.variation = currentItem.variation_id
+		lineItem.name = currentItem.name
+		lineItem.price = currentItem.price
+		lineItem.quantity = currentItem.quantity
+		lineItem.tax= currentItem.total_tax
+		lineItem.total = currentItem.total
+		productsList.push(lineItem);
+	}
+	orderPayload = {
+		event:'Order Completed',
+		userID:order.customer_id,
+		properties:{
+			order_id: order.transaction_id,
+			checkout_id: order.id,
+			total:order.total,
+			revenue:order.total,
+			shipping:order.shipping_total,
+			tax:order.total_tax,
+			discount:order.discount_total,
+			coupon:order.coupon_lines[0].id,
+			currency:order.currency,
+			products:productsList,
+		}
+	}
+	analytics.track(orderPayload);
 });
 
 module.exports = {segRouter};
