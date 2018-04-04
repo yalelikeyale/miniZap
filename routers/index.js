30a2786505f85c527539c53fde441cb48b7ffeda
yale <newmany89@gmail.com>
2018-04-04 13:45:43 +1000

    update

--- routers/index.js
+++ routers/index.js
@@ -1,6 +1,5 @@
 'use strict';
 const {loginRouter} = require('./signinRouter');
-const {podioRouter} = require('./podioRouter');
 const {userRouter} = require('./usersRouter');
 const {segRouter} = require('./segmentRouter');
 module.exports = {loginRouter, userRouter, segRouter};
